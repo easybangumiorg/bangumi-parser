4aba5031a377f8eaa82adbe051929fe3bc61b867
AyalaKaguya <ayalakaguya@outlook.com>
2025-06-26 19:30:48 +0800

    Release 1.0.5

--- src/bangumi_parser/__init__.py
+++ src/bangumi_parser/__init__.py
@@ -6,5 +6,5 @@ from .core import BangumiParser, SeriesInfo, BangumiInfo
 from .config import BangumiConfig
 from . import utils
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __all__ = ["BangumiParser", "BangumiConfig", "SeriesInfo", "BangumiInfo", "utils"]
